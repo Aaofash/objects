2cc64c0e9ed9289d8f78667bd169abbe127efe03
Aaofash <102664550+Aaofash@users.noreply.github.com>
2022-06-24 09:42:27 -0500

    stopwatch

--- int.js
+++ int.js
@@ -7,7 +7,7 @@
     }
   }
 }
-const circle = createCircle(1);*/
+const circle = createCircle(1);
 
 function Circle(radius) {
   this.radius = radius;
@@ -16,4 +16,29 @@ function Circle(radius) {
   }
 }
 Circle.call({}, 1, 2, 3)
-const another = new Circle(1);
+const another = new Circle(1);*/
+
+
+function drawCircle(radius, location) {
+  return {
+    radius,
+    location,
+    draw() {
+      console.log('draw');
+    }
+  }
+}
+
+const circle1 = drawCircle(1,2);
+const circle2 = drawCircle(2,2);
+console.log(circle1,circle2);
+
+function Circle(radius, location) {
+  this.radius = radius;
+  this.location = location;
+  this.draw = function() {
+    console.log('draw');
+  }
+}
+
+
